6f28d5f0242bf125d9621f99f3ea648e4f86f0a6
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2019-06-12 15:14:12 -0300

    Now I can run it locally with heroku. Also added a gitignore file.

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -1,15 +1,14 @@
 from flask import Flask
-app = Flask(__name__)
 from twilio.twiml.messaging_response import MessagingResponse
 
+app = Flask(__name__)
+
+
+def get_app ():
+    return app
 
 @app.route("/cardapio", methods=['POST', 'GET'])
 def hello():
     resp = MessagingResponse ()
     resp.message ("Arroz, feijão, batata e macarrão.")
     return str (resp)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
---
+++ run_app.py
@@ -0,0 +1,6 @@
+from receiver.receiver import get_app
+
+app = get_app ()
+
+if __name__ == "__main__":
+    app.run()
